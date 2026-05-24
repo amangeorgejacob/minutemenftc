0dbab0eb8d5d88b8ebf3ff8a75263509e6886a08
AJ <148181429+amangeorgejacob@users.noreply.github.com>
2026-05-24 20:44:53 +0000

    Updated Text

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -49,7 +49,7 @@ export default function Contact() {
           GET IN <span className="text-primary">TOUCH</span>
         </motion.h1>
         <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
-          Have FTC questions, want to learn more, or have website suggestions?
+          Have FTC questions, have website suggestions, or want to sponsor us?
           Please dont hesitate to send us a message!
         </p>
       </section>
